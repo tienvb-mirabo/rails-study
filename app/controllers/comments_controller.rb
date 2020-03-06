443ea23df032a8711fe5fcdee460bb3559f2575a
Vu Ba Tien <tienvb@mirabo.com.vn>
2020-03-06 10:34:25 +0700

    submit code

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.find(comment_params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+end
